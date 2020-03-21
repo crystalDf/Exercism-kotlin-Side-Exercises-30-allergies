051873b94a886df7d166012c895b1fbb40994591
crystalDf <chendong333@gmail.com>
2020-03-22 00:21:38 +0800

    30 allergies

--- src/main/kotlin/Allergies.kt
+++ src/main/kotlin/Allergies.kt
@@ -2,15 +2,11 @@ class Allergies(private val score: Int) {
 
     fun getList(): List<Allergen> {
 
-        return (score % 256).toBinaryString().reversed().mapIndexed { index, char ->
+        return (score % 256).toString(2).reversed().mapIndexed { index, char ->
             if (char == '1') Allergen.values()[index] else null }.filterNotNull()
     }
 
     fun isAllergicTo(allergen: Allergen): Boolean {
         return allergen in getList()
-    }
-
-    private fun Int.toBinaryString(): String {
-        return Integer.toBinaryString(this)
     }
 }
